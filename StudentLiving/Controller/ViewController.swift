26a2f16f2c1c89ccc58990025df3539df08210e7
Dan <dosullivan557@gmail.com>
2018-08-20 23:06:51 +0100

    Attempt to fix

--- StudentLiving/Controller/ViewController.swift
+++ StudentLiving/Controller/ViewController.swift
@@ -1,29 +1,57 @@
 //
-//  ViewController.swift
-//  UniHome
+//  CollectionViewController.swift
+//  StudentLiving
 //
-//  Created by Ileri Onawoga on 02/08/2018.
+//  Created by Danny on 20/08/2018.
 //  Copyright © 2018 Ileri Onawoga. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+private let reuseIdentifier = "Cell"
+
+class ViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-              navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action:  #selector(handleLogout))
-    }
-        @objc func handleLogout(){
-            let loginController = LoginController()
-            
-            present(loginController,animated: true, completion: nil)
-        }
+        
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action:  #selector(handleLogout))
+        
         
     }
     
-
-
+    @objc func handleLogout(){
+        let loginController = LoginController()
+        
+        present(loginController,animated: true, completion: nil)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor.blue
+        // Configure the cell
+        
+        return cell
+    }
+    
+    
+}
